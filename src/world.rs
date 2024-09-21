7633fbde8e9cefb17bd06fbb69890c4ce3901048
AudranTourneur <108300467+AudranTourneur@users.noreply.github.com>
2024-09-21 12:01:33 +0200

    Replaced the platform by a colored floor

--- src/world.rs
+++ src/world.rs
@@ -2,12 +2,14 @@ use crate::camera::CameraController;
 use crate::Player;
 use bevy::prelude::*;
 use bevy::prelude::{Commands, ResMut};
+use rand::Rng;
 
 pub fn setup_world(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    /*
     // Spawn the platform
     commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(Cuboid::new(20.0, 1.0, 20.0))),
@@ -15,7 +17,20 @@ pub fn setup_world(
         transform: Transform::from_translation(Vec3::new(0.0, -0.5, 0.0)),
         ..Default::default()
     });
+     */
 
+    let mut rng = rand::thread_rng();
+
+    for i in -10..11 {
+        for j in -10..11 {
+            commands.spawn(PbrBundle {
+                mesh: meshes.add(Mesh::from(Cuboid::new(1.0, 1.0, 1.0))),
+                material: materials.add(Color::srgb(rng.gen(), rng.gen(), rng.gen())),
+                transform: Transform::from_translation(Vec3::new(i as f32, -0.5, j as f32)),
+                ..Default::default()
+            });
+        }
+    }
     // Spawn the player
     commands
         .spawn(PbrBundle {
