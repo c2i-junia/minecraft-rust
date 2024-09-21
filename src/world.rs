0ed347ed167dc6f0e3347fdce4a19c6d47d9c3f4
AudranTourneur <108300467+AudranTourneur@users.noreply.github.com>
2024-09-21 11:46:43 +0200

    Update bevy to version 0.14

--- src/world.rs
+++ src/world.rs
@@ -11,7 +11,7 @@ pub fn setup_world(
     // Spawn the platform
     commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(Cuboid::new(20.0, 1.0, 20.0))),
-        material: materials.add(Color::rgb(0.75, 0.75, 0.75)),
+        material: materials.add(Color::srgb(0.75, 0.75, 0.75)),
         transform: Transform::from_translation(Vec3::new(0.0, -0.5, 0.0)),
         ..Default::default()
     });
@@ -20,7 +20,7 @@ pub fn setup_world(
     commands
         .spawn(PbrBundle {
             mesh: meshes.add(Mesh::from(Cuboid::new(1.0, 1.0, 1.0))),
-            material: materials.add(Color::rgb(1.0, 0.0, 0.0)),
+            material: materials.add(Color::srgb(1.0, 0.0, 0.0)),
             transform: Transform::from_translation(Vec3::new(0.0, 0.5, 0.0)),
             ..Default::default()
         })
