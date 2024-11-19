83c2ae0e168e62b73b6277ab4e6b4b6ddd754f3a
AudranTourneur <108300467+AudranTourneur@users.noreply.github.com>
2024-11-19 13:55:18 +0100

    build: Setup nightly toolchain with cranelift for faster compilation

--- run-client-x.sh
+++ run-client-x.sh
@@ -7,4 +7,4 @@ if [[ -z "$1" ]]; then
   exit 1
 fi
 
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(rustc --print sysroot)/lib:$PWD/target/debug/deps RUST_BACKTRACE=1 RUST_LOG=client=debug,server=debug,shared=debug,warn ./minecraft-rust-client-$1/bin/minecraft-rust
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(rustc --print sysroot)/lib:$PWD/target/debug/deps:$(rustc --print sysroot)/lib/rustlib/x86_64-unknown-linux-gnu/lib RUST_BACKTRACE=1 RUST_LOG=client=debug,server=debug,shared=debug,warn ./minecraft-rust-client-$1/bin/minecraft-rust
--- run-server.sh
+++ run-server.sh
@@ -4,4 +4,4 @@ sh make-if-needed.sh
 
 PORT=${1:-8000}
 
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(rustc --print sysroot)/lib:$PWD/target/debug/deps RUST_BACKTRACE=1 RUST_LOG=server=debug,shared=debug,warn ./minecraft-rust-server/bin/minecraft-rust-server --port $PORT
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(rustc --print sysroot)/lib:$PWD/target/debug/deps:$(rustc --print sysroot)/lib/rustlib/x86_64-unknown-linux-gnu/lib RUST_BACKTRACE=1 RUST_LOG=server=debug,shared=debug,warn ./minecraft-rust-server/bin/minecraft-rust-server --port $PORT
